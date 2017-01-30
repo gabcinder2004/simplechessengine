2cfe6a9bdbd8cacc5e05b07a2b7919eb1483beb3
Gab <gabcinder2004@yahoo.com>
2017-01-29 21:53:20 -0500

    Some more work done

--- SimplifiedChessEngine/Program.cs
+++ SimplifiedChessEngine/Program.cs
@@ -17,7 +17,7 @@ namespace SimplifiedChessEngine
                 solver.NextPlay(game.ChessBoard, ChessColor.White, new List<ChessMove>());
                 gameSolutions.Add(solver);
                 //var str = string.Format("{0} : {1}", solver.GameOver ? "YES" : "NO", solver.Stopwatch.Elapsed);
-                Console.WriteLine(solver.Winner == ChessColor.White ? "YES" : "NO");
+                Console.WriteLine(solver.Results.Any(x => x.Winner == ChessColor.White) ? "YES" : "NO");
             }
 
             Console.ReadLine();
@@ -393,107 +393,83 @@ namespace SimplifiedChessEngine
         }
     }
 
+    public class GameResult
+    {
+        public List<ChessMove> AllMoves { get; set; }
+        public ChessColor Winner { get; set; }
+
+        public override string ToString()
+        {
+            return Winner.ToString();
+        }
+    }
+
     public class GameSolver
     {
-        public bool GameOver;
-
         public ChessColor Winner = ChessColor.White;
         public ChessGame Game { get; set; }
         public List<ChessMove> WinningMoves { get; set; }
         public Stopwatch Stopwatch { get; set; }
 
+        public List<GameResult> Results { get; set; }
+
         public GameSolver(ChessGame game)
         {
             Game = game;
             WinningMoves = new List<ChessMove>();
+            Results = new List<GameResult>();
         }
 
-        public List<ChessMove> NextPlay(ChessBoard board, ChessColor colorTurn, List<ChessMove> allMoves)
+        public void NextPlay(ChessBoard board, ChessColor colorTurn, List<ChessMove> allMoves)
         {
-            var moves = new List<ChessMove>();
-
-            if (GameOver)
+            if (Results.Any(x => x.Winner == ChessColor.White))
             {
-                return moves;
+                return;
             }
 
             if ((allMoves.Count() >= Game.TotalMovesAllowed) || (colorTurn == ChessColor.Black && allMoves.Count == Game.TotalMovesAllowed - 1))
             {
-                Winner = ChessColor.Black;
-                GameOver = true;
-                WinningMoves = allMoves;
-                return WinningMoves;
+                Results.Add(new GameResult { AllMoves = allMoves, Winner = ChessColor.Black });
+                return;
             }
 
+            var nextColor = colorTurn == ChessColor.White ? ChessColor.Black : ChessColor.White;
             var cells = board.Cells.Where(cell => !cell.IsEmpty() && cell.Piece.Color == colorTurn).OrderBy(x => x.Piece.GetType() != typeof(Queen)).ToList();
+            cells.Where(x => !x.IsEmpty()).ToList().ForEach(c => c.Piece.GetAvailableMoves(c, board));
 
-            //Can white win first turn?
-            if (allMoves.Count == 0)
+            var allMovesThisTurn = cells.Where(x => !x.IsEmpty() && x.Piece.Color == colorTurn).SelectMany(x => x.Piece.AvailableMoves).ToList();
+
+            if (!allMovesThisTurn.Any())
             {
-                cells.Where(x => !x.IsEmpty()).ToList().ForEach(c => c.Piece.GetAvailableMoves(c, board, false));
+                Results.Add(new GameResult() { AllMoves = allMoves, Winner = nextColor });
+                return;
+            }
 
-                var cell = cells.FirstOrDefault(x => !x.IsEmpty() && x.Piece.AvailableMoves.Any(m => !m.To.IsEmpty() && m.To.Piece.GetType() == typeof(Queen) && m.Action == ChessAction.KILL));
+            if (allMovesThisTurn.Any(x => !x.To.IsEmpty() && x.To.Piece.GetType() == typeof(Queen) && x.Action == ChessAction.KILL))
+            {
+                var move = allMovesThisTurn.First(x => !x.To.IsEmpty() && x.To.Piece.GetType() == typeof(Queen) && x.Action == ChessAction.KILL);
+                allMoves.Add(move);
+                Results.Add(new GameResult() { AllMoves = allMoves, Winner = colorTurn });
+                return;
+            }
 
-                if (cell != null)
+            foreach (var chessMove in allMovesThisTurn)
+            {
+                var currentMoves = new List<ChessMove>();
+                currentMoves.AddRange(allMoves);
+                var newBoard = (ChessBoard)board.Clone();
+
+                newBoard.MakeMove(chessMove);
+                currentMoves.Add(chessMove);
+
+                NextPlay(newBoard, nextColor, currentMoves);
+
+                if (Results.Any(x => x.Winner == ChessColor.White))
                 {
-                    var move = cell.Piece.AvailableMoves.First(m => !m.To.IsEmpty() && m.To.Piece.GetType() == typeof(Queen) && m.Action == ChessAction.KILL);
-                    Winner = ChessColor.White;
-                    GameOver = true;
-                    WinningMoves.Add(move);
-                    return WinningMoves;
+                    return;
                 }
             }
 
-            foreach (var cell in cells)
-            {
-                var availableMoves = cell.Piece.GetAvailableMoves(cell, board);
-
-                // if it is possible to kill the opposing queen, do it.
-                if (availableMoves.Any(move => !move.To.IsEmpty() && move.To.Piece.GetType() == typeof(Queen) && move.Action == ChessAction.KILL))
-                {
-                    var move = availableMoves.First(m => !m.To.IsEmpty() && m.To.Piece.GetType() == typeof(Queen) && m.Action == ChessAction.KILL);
-
-                    // If our queen is going to die, abandon this move path
-                    if (colorTurn != ChessColor.Black) return new List<ChessMove>();
-
-                    // If we can kill the black queen, do it and win.
-                    allMoves.Add(move);
-                    GameOver = true;
-                    Winner = ChessColor.Black;
-                    WinningMoves = allMoves;
-                    return WinningMoves;
-                }
-
-                //Queen cannot be killed this turn, find next move.
-                foreach (var move in availableMoves)
-                {
-                    var currentMoves = new List<ChessMove>();
-                    currentMoves.AddRange(allMoves);
-                    var newBoard = (ChessBoard)board.Clone();
-
-                    newBoard.MakeMove(move);
-                    currentMoves.Add(move);
-
-                    var nextColor = colorTurn == ChessColor.White ? ChessColor.Black : ChessColor.White;
-                    var m = NextPlay(newBoard, nextColor, currentMoves);
-
-                    if (GameOver)
-                    {
-                        return m;
-                    }
-                }
-            }
-
-            // If it's blacks turn and they don't have any valid moves.. checkmate.
-            if (colorTurn == ChessColor.White && !cells.Any(cell => cell.Piece.AvailableMoves.Count > 0))
-            {
-                WinningMoves = allMoves;
-                GameOver = true;
-                Winner = ChessColor.Black;
-                return WinningMoves;
-            }
-
-            return new List<ChessMove>();
         }
     }
 
